afe8fc448aca71925b85bbae652d8cdc520f1e02
jawad saleem <jawadsalim12@gmail.com>
2020-12-10 15:47:08 -0600

    error handling

---
+++ error_handling/calculator_multiple_errors.py
@@ -0,0 +1,16 @@
+# sum of two numbers, both numbers should be int
+def calc(num1, num2, op):
+    try:
+        if op == '+':
+            return num1+num2
+        elif op == '/':
+            return num1/num2
+        else:
+            return "operation not defined"
+    except (TypeError, ZeroDivisionError) as err:
+        return f'Both numbers should be integers:\nError: {err}'
+    else:
+        return "im in else"
+
+
+print(calc(10, 's', '/'))
---
+++ error_handling/try_except_else_finally.py
@@ -0,0 +1,18 @@
+# syntax, key, index,key, zeroDivsion, Type, value
+
+# Error Handling
+# This example only accepts int as an age
+while True:
+    try:
+        age = int(input('whats ur age'))
+        print(age)
+        # 10/age
+    except ValueError:
+        print("enter valid input")
+    except ZeroDivisionError:
+        print("enter non 0 number")
+    else:
+        print("thank you")
+        break
+    finally:
+        print("im finally")
