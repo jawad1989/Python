b88dcd830eb632efb52b368b59676f17b657fa73
jawad saleem <jawadsalim12@gmail.com>
2020-12-13 01:07:39 -0600

    json

---
+++ json/json_load.py
@@ -0,0 +1,8 @@
+import json
+
+filename = 'json/json_write.json'
+
+with open(filename) as f:
+    data = json.load(f)
+
+print(data)
---
+++ json/json_write_file.py
@@ -0,0 +1,28 @@
+import json
+
+my_list = [1, 2, 3, 4, 5, 6]
+my_dict = {
+    'user_a': {
+        'fname': 'jawad',
+        'lname': 'saleem',
+        'city': 'lahore',
+        'age': 31},
+    'user_b': {
+        'fname': 'rajab',
+        'lname': 'saleem',
+        'city': 'lahore',
+        'age': 26},
+    'user_c': {
+        'fname': 'muhammad',
+        'lname': 'saleem',
+        'city': 'lahore',
+        'age': 60},
+    'user_d': {
+        'fname': 'ahmad',
+        'lname': 'jawad',
+        'city': 'lahore',
+        'age': 1}
+}
+filename = 'json/json_write.json'
+with open(filename, 'w') as file_object:
+    json.dump(my_dict, file_object)
