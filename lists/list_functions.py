6978c30a03de6d76c2fb14add49b3cf638058fba
jawad1989 <jawadsalim12@gmail.com>
2021-04-12 12:36:53 -0500

    new funcs added

---
+++ lists/list_functions.py
@@ -0,0 +1,33 @@
+list_1 = [1,2,3,4,5]
+list_2 = ["a","b","c"]
+list_3 = [1,"x",2,"y"]
+list_4 = []
+
+# append
+list_1.append(6)
+print (f"append: {list_1}")
+
+# extend
+
+# copy
+# list_1.copy(list_4)
+list_4 = list_1.copy()
+print(f"copy: {list_4}")
+
+# clear
+
+# count
+
+# extent
+
+# Index
+
+# insert
+
+# pop
+
+# remove
+
+# reverse
+
+# sort
